5c15ad0269e60a0a3695295c5d5d3f68bab6f5cf
RorySchof <roryschofield@gmail.com>
2023-03-19 21:32:41 -0400

    added testing

--- timer1.js
+++ timer1.js
@@ -1,22 +1,18 @@
 
 const number =  process.argv.slice(2)
-
-
 function newBeep (){
-
   process.stdout.write('\x07');
-
 }
-
-
 function alarmClock (delay){
   delay = Number(delay) 
   setTimeout(newBeep, delay * 1000)
 
 } 
 for (const num of number){
-  alarmClock(num)
-
-
+  let delay = Number(num);
+  if (!isNaN(delay) && delay >= 0) {
+    alarmClock(delay)
+  }
 }
 
+//
